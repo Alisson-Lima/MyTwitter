f367944d226bff6012bc90fe128cae8f66922576
Alisson Lima <renalima.rl46@gmail.com>
2023-10-11 21:45:00 -0300

    Fixing timeline scroll thumb and adding come back to top button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -9,18 +9,45 @@ import Tweet from '../../components/Tweet'
 import "../../css/index.css"
 import styles from "./style.module.css"
 import FirstTweetMessage from '../../components/FirstTweetMessage'
+import { useRef, useState, useEffect } from 'react'
 
 const Home = () => {
+
+  const mainContainerRef = useRef(); // Use mainContainerRef em vez de pageScrollElementRef
+  const [showScrollButton, setShowScrollButton] = useState(false);
 
   // hooks
   const {tweets, loading} = useGetTweets("tweets")
 
+  const handleScrollTop = ()=>{
+    mainContainerRef.current.scrollTo(0,0)
+  }
+
+    // Verifique a posição da timeline no elemento .main_container
+    const handleScroll = () => {
+      const mainContainer = mainContainerRef.current;
+      if (mainContainer.scrollTop > mainContainer.clientHeight) {
+        setShowScrollButton(true);
+      } else {
+        setShowScrollButton(false);
+      }
+    };
+  
+    useEffect(() => {
+      const mainContainer = mainContainerRef.current;
+      mainContainer.addEventListener('scroll', handleScroll);
+  
+      return () => {
+        mainContainer.removeEventListener('scroll', handleScroll);
+      };
+    }, []);
+
   return (
     <div>
-      <main className="main_container">
+      <main className="main_container" ref={mainContainerRef}>
         <div className="main_container_inbox">
 
-          <TypeTweet/>
+          <TypeTweet id="type"/>
 
           <hr />
 
@@ -38,6 +65,16 @@ const Home = () => {
             }
           </div>
         </div>
+
+        <button
+          onClick={() => handleScrollTop()}
+          className={styles.scrollTopButton + " " + (showScrollButton && styles.show)}
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none">
+            <path d="M2 18.4161L10 11.708L18 18.4161M2 8.70807L10 2L18 8.70807" stroke="#393640" strokeWidth="3" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+        </button>
+        
       </main>
     </div>
   )
